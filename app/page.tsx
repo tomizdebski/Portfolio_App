04a7963024dd2a1a68c93af9ca8abd328c3c8af8
tomizdebski <tomizdebski@gmail.com>
2023-12-08 20:46:43 +0100

    add_hero_content_component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from '@/components/main/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className='h-full w-full'>
-      <div className='flex flex-col gap-20'>
-
+      <div className='flex h-[850px] flex-col gap-20'>
+        <Hero />
       </div>
     </main>
   )
---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+const HeroContent = () => {
+  return (
+    <motion.div>
+
+    </motion.div>
+  )
+}
+
+export default HeroContent
